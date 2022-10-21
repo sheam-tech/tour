5a2338779c8dc244f5ffa1b098199d09d0f139a4
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-10-21 12:11:52 +0600

    add

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
+/**
+ * Title: Tour Management System
+ * Description: Create a system that contains several endpoints.
+ * Author: Hasibul Islam
+ * Date: 20/09/2022
+ */
+
+/* external imports */
+const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
-const colors = require("colors");
-const app = require("./app");
-const errorHandler = require("./middleware/errorHandler");
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
 
-// Mongoose Connect
-mongoose.connect(process.env.DB_URI).then(() => {
-  console.log("Database connected".yellow.bold);
-});
+/* internal imports */
+const errorHandler = require("./middlewares/error.middleware");
+// const dbConnection = require("./utils/db.util");
+const toursRouter = require("./routes/tours.route");
+const tourRouter = require("./routes/tour.route");
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`.red.bold);
-});
+/* application connection */
+const app = express();
+const port = process.env.PORT;
 
+/* middleware connection */
+app.use(cors());
+app.use(express.json());
+
+/* router connection */
+app.use("/tours", toursRouter);
+app.use("/tour", tourRouter);
+
+/* global error handler */
 app.use(errorHandler);
+
+/* DB connection */
+// dbConnection();
+mongoose
+  .connect(process.env.DB_URI, {
+    dbName: "tour-management-system",
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("TMS connect with MongoDB through Mongoose.");
+  });
+
+/* enable backend */
+app.get("/", (req, res) => {
+  res.status(200).json({
+    acknowledgement: true,
+    title: "Tour Management System",
+    message: "OK",
+    description: "TMS request on server working.",
+  });
+});
+
+/* enable port */
+app.listen(port, () => {
+  console.log(`TMS listening on port ${port}.`);
+});
--- middleware/errorHandler.js
+++
@@ -1,5 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    res.status(400).send(err);
-  };
-  
-  module.exports = errorHandler;
